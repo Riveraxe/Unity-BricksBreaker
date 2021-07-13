3d8e47af80f306563c4945302818210722b9fe5c
jiangzhenyu <jiangzhenyu4@gmail.com>
2021-07-14 01:27:14 +0800

    添加小球及挡板

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    private Rigidbody2D _self;
+
+    private Vector2 NowSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _self = GetComponent<Rigidbody2D>();
+        _self.velocity = new Vector2(0, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_self.velocity);
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D _self;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _self = GetComponent<Rigidbody2D>();
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float HorzonInput = Input.GetAxis("Horizontal");
+        _self.velocity = new Vector2(HorzonInput * speed, _self.velocity.y);
+    }
+}
